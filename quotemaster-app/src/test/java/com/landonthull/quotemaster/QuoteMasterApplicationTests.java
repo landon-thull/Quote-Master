16402ca1f3173f4a72b0336faf6d1c659dae5ed8
Landon Thull <landonthull.tx@gmail.com>
2023-07-30 09:12:27 -0500

    init

---
+++ quotemaster-app/src/test/java/com/landonthull/quotemaster/QuoteMasterApplicationTests.java
@@ -0,0 +1,14 @@
+package com.landonthull.quotemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteMasterApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+}
+
