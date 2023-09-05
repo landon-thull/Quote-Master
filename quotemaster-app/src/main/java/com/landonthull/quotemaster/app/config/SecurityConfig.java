d8ba0b5709a5120a0a843ff232929ac86e746a0b
Landon Thull <landonthull.tx@gmail.com>
2023-09-05 14:07:37 -0500

    chore: temporarily disable csrf and cors

--- quotemaster-app/src/main/java/com/landonthull/quotemaster/app/config/SecurityConfig.java
+++ quotemaster-app/src/main/java/com/landonthull/quotemaster/app/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.landonthull.quotemaster.app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,9 +12,12 @@ public class SecurityConfig {
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests((authz) -> authz
-        .anyRequest().permitAll()
-    );
+    http
+        .csrf(CsrfConfigurer::disable)
+        .cors(CorsConfigurer::disable)
+        .authorizeHttpRequests((authz) -> authz
+            .requestMatchers("/**", "/users", "/users/**").permitAll()
+        );
     return http.build();
   }
 }
