4b472ca6781cfa4eb3fd968b695f5b6ece583824
Landon Thull <landonthull.tx@gmail.com>
2023-09-05 13:42:13 -0500

    chore: move component scan to config

--- quotemaster-app/src/main/java/com/landonthull/quotemaster/app/QuoteMasterApplication.java
+++ quotemaster-app/src/main/java/com/landonthull/quotemaster/app/QuoteMasterApplication.java
@@ -3,10 +3,7 @@ package com.landonthull.quotemaster.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {
-    "com.landonthull.quotemaster.app",
-    "com.landonthull.quotemaster.core",
-    "com.landonthull.quotemaster.infrastructure.common"})
+@SpringBootApplication
 public class QuoteMasterApplication {
 
   public static void main(String[] args) {
