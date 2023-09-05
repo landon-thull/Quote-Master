5fad0a251b2f616959bc477b26fa2056499259a0
Landon Thull <landonthull.tx@gmail.com>
2023-09-04 20:50:16 -0500

    chore: move application

--- quotemaster-app/src/main/java/com/landonthull/quotemaster/app/QuoteMasterApplication.java
+++ quotemaster-app/src/main/java/com/landonthull/quotemaster/app/QuoteMasterApplication.java
@@ -1,13 +1,15 @@
-package com.landonthull.quotemaster;
+package com.landonthull.quotemaster.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+    "com.landonthull.quotemaster.app",
+    "com.landonthull.quotemaster.core",
+    "com.landonthull.quotemaster.infrastructure.common"})
 public class QuoteMasterApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(QuoteMasterApplication.class, args);
   }
-
 }
