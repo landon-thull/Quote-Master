720625928ba614f5053aaf1e09fa3abd0ac88c7c
Landon Thull <landonthull.tx@gmail.com>
2023-09-06 11:02:07 -0500

    fix: use passwordEncoder bean

--- quotemaster-app/src/main/java/com/landonthull/quotemaster/app/config/SecurityConfig.java
+++ quotemaster-app/src/main/java/com/landonthull/quotemaster/app/config/SecurityConfig.java
@@ -2,13 +2,21 @@ package com.landonthull.quotemaster.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -16,8 +24,15 @@ public class SecurityConfig {
         .csrf(CsrfConfigurer::disable)
         .cors(CorsConfigurer::disable)
         .authorizeHttpRequests((authz) -> authz
+            .requestMatchers("/login").permitAll()
             .requestMatchers("/**", "/users", "/users/**").permitAll()
         );
     return http.build();
+  }
+
+  @Bean
+  public AuthenticationManager
+  authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    return configuration.getAuthenticationManager();
   }
 }
--- quotemaster-infrastructure/src/main/java/com/landonthull/quotemaster/infrastructure/common/PasswordEncoderImpl.java
+++ quotemaster-infrastructure/src/main/java/com/landonthull/quotemaster/infrastructure/common/PasswordEncoderImpl.java
@@ -1,7 +1,6 @@
 package com.landonthull.quotemaster.infrastructure.common;
 
 import com.landonthull.quotemaster.core.user.port.in.PasswordEncoder;
-import java.security.SecureRandom;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +9,8 @@ public class PasswordEncoderImpl implements PasswordEncoder {
 
   private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-  public PasswordEncoderImpl() {
-    this.bCryptPasswordEncoder = new BCryptPasswordEncoder(10, new SecureRandom());
+  public PasswordEncoderImpl(BCryptPasswordEncoder bCryptPasswordEncoder) {
+    this.bCryptPasswordEncoder = bCryptPasswordEncoder;
   }
 
   @Override
