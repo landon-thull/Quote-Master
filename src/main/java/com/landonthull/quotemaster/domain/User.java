7f768e3aeb98f84025fe8650da21865e81938977
Landon Thull <landonthull.tx@gmail.com>
2023-10-07 03:10:12 -0500

    feat: add auto gen strategy for user id

--- src/main/java/com/landonthull/quotemaster/domain/User.java
+++ src/main/java/com/landonthull/quotemaster/domain/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.PrePersist;
@@ -22,6 +24,7 @@ import java.sql.Timestamp;
 public class User {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id")
   private Long id;
 
