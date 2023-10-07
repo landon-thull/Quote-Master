c015ae44a030c49d3cb5d4ee9cd37f682a7517c1
Landon Thull <landonthull.tx@gmail.com>
2023-10-07 04:17:00 -0500

    feat: add login endpoint

---
+++ src/main/java/com/landonthull/quotemaster/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.landonthull.quotemaster.controller;
+
+import com.landonthull.quotemaster.dto.SignInRequest;
+import com.landonthull.quotemaster.dto.SignInResponse;
+import com.landonthull.quotemaster.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+  private final AuthService authService;
+
+  public AuthController(AuthService authService) {
+    this.authService = authService;
+  }
+
+  @PostMapping("/login")
+  public SignInResponse login(@Valid @RequestBody SignInRequest request) {
+    return authService.signIn(request);
+  }
+}
