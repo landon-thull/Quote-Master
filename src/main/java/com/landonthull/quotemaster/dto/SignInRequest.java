9e8cf30f9c6a7966828d7fd2adea5933e5bfaf46
Landon Thull <landonthull.tx@gmail.com>
2023-10-08 04:19:46 -0500

    feat: add validation to sign in request

--- src/main/java/com/landonthull/quotemaster/dto/SignInRequest.java
+++ src/main/java/com/landonthull/quotemaster/dto/SignInRequest.java
@@ -1,13 +1,17 @@
 package com.landonthull.quotemaster.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public class SignInRequest {
 
-  @NotBlank(message = "Email is required.")
+  @NotBlank(message = "Email is required")
+  @Email(message = "Email must be correctly formatted")
   private final String email;
 
   @NotBlank(message = "Password is required")
+  @Size(min = 8, message = "Password must be 8 characters or longer")
   private final String password;
 
   public SignInRequest(String email, String password) {
